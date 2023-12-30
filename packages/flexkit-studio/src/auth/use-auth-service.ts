edd07c6428a4dbb6ca9ea5be4111c5793d8780c9
Daniel Kratohvil <daniel@naturitas.es>
2023-12-30 01:44:35 +0100

    Add schema to useAuth hook

--- packages/flexkit-studio/src/auth/use-auth-service.ts
+++ packages/flexkit-studio/src/auth/use-auth-service.ts
@@ -3,20 +3,17 @@
 import useSWR from 'swr';
 import { useParams } from 'react-router-dom';
 import { apiPaths } from '../core/api-paths';
-// eslint-disable-next-line -- remove this after removing the temp schema
 import type { AuthService, ProjectConfig, User } from './types';
-// import { schema } from './temp-schema'; // TODO: delete this once finished testing the new schema
 
 export default function useAuthService(): AuthService {
   const { projectId } = useParams();
   const { data: user, isLoading: isLoadingUser } = useSWR(apiPaths(projectId ?? '').currentUser, (url: string) =>
     fetch(url, { mode: 'cors' }).then((res) => res.json() as Promise<User>)
   );
-  // const { data: schema, isLoading: isLoadingSchema } = useSWR(
-  //   () => (user ? apiPaths.schema : null),
-  //   (url: string) => fetch(url, { mode: 'cors' }).then((res) => res.json() as Promise<ProjectConfig>)
-  // );
-  const isLoadingSchema = false; // TODO: delete this once finished testing the new schema
+  const { data: schema, isLoading: isLoadingSchema } = useSWR(
+    () => (user?.id ? apiPaths(projectId ?? '').schema : null),
+    (url: string) => fetch(url, { mode: 'cors' }).then((res) => res.json() as Promise<ProjectConfig>)
+  );
   const isLoading = isLoadingUser || isLoadingSchema;
 
   return [
@@ -24,8 +21,8 @@ export default function useAuthService(): AuthService {
     {
       user,
       projectConfig: {
-        jsonSchema: [], // schema?.jsonSchema || [],
-        scopes: {}, // schema?.scopes || {},
+        jsonSchema: schema?.jsonSchema || [],
+        scopes: schema?.scopes || {},
       },
       logout: async () => {
         // wipe remote session
--- packages/flexkit-studio/src/ui/containers/layout.tsx
+++ packages/flexkit-studio/src/ui/containers/layout.tsx
@@ -1,9 +1,6 @@
 import { Outlet, useParams } from 'react-router-dom';
-import useSWR from 'swr';
-import type { Fetcher } from 'swr';
 import { useAuth } from '../../auth/auth-context';
 import { Login } from '../../auth/login';
-import { apiPaths } from '../../core/api-paths';
 import { useConfig } from '../../core/config/config-context';
 import { Loading } from '../components/loading';
 import { AppBar } from './appbar';
@@ -17,30 +14,18 @@ type Props = {
   };
 };
 
-const fetcher = (url: string): Promise<Fetcher> =>
-  fetch(url, { mode: 'cors' }).then((res) => res.json() as Promise<Fetcher>);
-
 export function Layout({ version }: Props): JSX.Element {
   const { contributions, projects } = useConfig();
   const [isLoading, auth] = useAuth();
   const { projectId } = useParams();
   const { apps } = contributions;
-  const {
-    data: schema,
-    isLoading: isLoadingSchema,
-    error,
-  } = useSWR(() => (auth.user?.id ? apiPaths(projectId ?? '').schema : null), fetcher);
 
-  if (isLoading || isLoadingSchema) {
+  if (isLoading) {
     return <Loading />;
   }
 
   if (!auth.user?.id) {
     return <Login projectId={projectId ?? ''} />;
-  }
-
-  if (error) {
-    throw new Error('Not found');
   }
 
   return (
@@ -49,7 +34,6 @@ export function Layout({ version }: Props): JSX.Element {
       <div className="fk-flex fk-h-full fk-max-h-[calc(100%-3.5rem)] fk-border-t fk-border-border fk-grow shrink">
         <AppBar apps={apps} version={version} />
         <div className="fk-w-full focus-visible:fk-outline-none focus-visible:fk-ring-0 focus-visible:fk-ring-offset-0 fk-overflow-x-hidden fk-overflow-y-auto">
-          {JSON.stringify(schema)}
           <Outlet />
         </div>
       </div>
