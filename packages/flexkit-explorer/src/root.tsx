334ecc39caaf289e1f46617543ec08c4f7edbbd6
Daniel Kratohvil <danielkratohvil@gmail.com>
2025-04-25 09:06:14 +0200

    Route GraphiQL Explorer calls through the Studio Next.js proxy to append Auth header

--- packages/flexkit-explorer/src/root.tsx
+++ packages/flexkit-explorer/src/root.tsx
@@ -8,7 +8,7 @@ import '@graphiql/react/dist/style.css';
 
 export function Root(): JSX.Element {
   const { currentProjectId } = useConfig();
-  const url = `https://${currentProjectId}.api.flexkit.io/graphql`;
+  const url = `/api/flexkit/${currentProjectId}/graphql`;
   const fetcher = useMemo(() => createGraphiQLFetcher({ url }), [url]);
 
   return <GraphiQL fetcher={fetcher} forcedTheme="light" />;
